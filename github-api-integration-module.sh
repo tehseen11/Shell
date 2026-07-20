27de09421a1233901b906f6d0c54e9244eee39e7
Tehseen Nayeem Khan <127304318+tehseen11@users.noreply.github.com>
2026-07-20 18:40:38 +0530

    Update usage instructions in GitHub API script
    Removed usage instructions for GitHub token input.

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -7,7 +7,6 @@
 #
 # This script will help users to communicate and retrieve information from GitHub
 # Usage:
-#   Please provide your github token and rest api to the script as input
 #
 #
 ################################
